71faee000d9e72cf1c610fc1b10c7d05b9e33702
maoyamagishi <yamagishi.m.ae@m.titech.ac.jp>
2024-09-16 22:03:21 +0900

    not Enough 1st

---
+++ src/OneObjTest.py
@@ -0,0 +1,56 @@
+from ThermoModel import obj
+import matplotlib.pyplot as plt
+#initial condition
+pilot = obj(1,1000.0,1.0,1.0)
+fairing = obj(1,1.0,1.0,0.0)
+dt = 0.01  #Global time interval
+heatgen = 400  #(W) generating heat from pilot
+
+for ii in range(200):
+    pilot.save()
+    fairing.save()
+    #pilot.heatup(heatgen,dt)
+    #print("pilot heat up!")
+    pilotrad = pilot.radiate(fairing.kelv,dt,1)
+    fairingrad = fairing.radiate(pilot.prekel,dt,1)
+    # if fairing.kelv > 306.0:
+    #     fairing.kelv = 306.0
+
+plt.plot(pilot.hstlst,label = "pilot")
+plt.plot(fairing.hstlst,label = "fairing")
+
+pilot2 = obj(5,1.0,0.8,303.0)
+fairing2 = obj(5,1.0,0.8,303.0)
+
+# pilot2 = obj(1,70.0,1.0,363.0)
+# fairing2 = obj(1,1.0,1.0,303.0)
+# for ii in range(40):
+#     pilot2.save()
+#     fairing2.save()
+#     #pilot2.heatup(heatgen,dt)
+#     #print("pilot heat up!")
+#     pilot2.radiate(fairing2.kelv,dt,1)
+#     #print("pilot radiate!")
+#     # fairing.heatup(-1*pilotrad,dt)
+#     fairing2.radiate(pilot2.prekel,dt,0.1)
+#     # if fairing.kelv > 306.0:
+#     #     fairing.kelv = 306.0
+
+# for ii in range(40):
+#     pilot2.save()
+#     fairing2.save()
+#     pilot2.heatup(heatgen,dt)
+#     #print("pilot heat up!")
+#     pilotrad = pilot2.radiate(fairing2.kelv,dt,1)
+#     print("pilot radiate!")
+#     fairingrad = fairing2.radiate(pilot2.prekel,dt,0.1)
+#     print("fairing radiate!")
+#     fairing2.heatup(-1*pilotrad,dt)
+#     pilot2.heatup(-1*fairingrad,dt)
+
+# plt.plot(pilot2.hstlst,label = "pilot2")
+# plt.plot(fairing2.hstlst,label = "fairing2")
+plt.legend()
+plt.show()
+for ii in range(len(fairing.hstlst)):
+    print(f"{fairing.hstlst[ii]}")
---
+++ src/ThermoModel.py
@@ -0,0 +1,54 @@
+class obj():
+    sigma = 5.67 * 10**(-8) #Stefan-Boltzmann constant
+    # sigma = 1.0#for test
+
+    def __init__(self,specheat:float,mass:float,eps:float,kelv:float) -> None:
+        self.__specheat = specheat
+        self.__mass = mass
+        self.__eps = eps    #emissivity
+        self.__kelv = kelv  #(K) Kelvin
+        self.__prekel = 0
+        self.__hstlst = []   #history of tempreture of this object
+
+    @property
+    def specheat(self):
+        return self.__specheat
+    
+    @property
+    def mass(self):
+        return self.__mass
+    
+    @property
+    def eps(self):
+        return self.__eps
+    
+    @property
+    def kelv(self):
+        return self.__kelv
+    
+    @kelv.setter
+    def kelv(self, value):
+        self.__kelv = value
+    
+    @property
+    def hstlst(self):
+        return self.__hstlst
+    
+    @property
+    def prekel(self):
+        return self.__prekel
+    
+    def heatup(self,Q:float,dt:float)->None:
+        self.__prekel = self.__kelv
+        self.__kelv += Q*dt/(self.__mass*self.__specheat)
+        print(Q*dt/(self.__mass*self.__specheat))
+
+    def radiate(self,compereKelv,dt,rate)->float:
+        selfKelv = self.__kelv
+        amtheat = self.__eps*self.sigma*rate*(-selfKelv**4 + compereKelv**4)
+        print(amtheat)
+        self.heatup(amtheat,dt)
+        return amtheat
+    
+    def save(self)->None:
+        self.__hstlst.append(self.__kelv)
